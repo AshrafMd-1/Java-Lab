8176fcf9592788252c4fb42ecad1cfc91af91492
Ashraf Mohammed <98876115+AshrafMd-1@users.noreply.github.com>
2022-12-31 05:19:38 +0000

    My first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+1. create file in java CreateFile.java
+2. write to a file WriteToFile.java
+3. get info of the file GetFileInfo.java
+4. get number of lines LineNum.java [syntax: java LineNum filename.txt]
+5. get word count of file WordCountInFile.java
---
+++ WriteToFile.java
@@ -0,0 +1,16 @@
+import java.io.FileWriter; // Import the FileWriter class
+import java.io.IOException; // Import the IOException class to handle errors
+
+public class WriteToFile {
+  public static void main(String[] args) {
+    try {
+      FileWriter myWriter = new FileWriter("filename.txt");
+      myWriter.write("File creation completed, in that text file i need to write !");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
