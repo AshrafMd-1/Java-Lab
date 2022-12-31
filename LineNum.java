3c145427b687fa29295705303a4f4f2ade5473d0
Ashraf Mohammed <98876115+AshrafMd-1@users.noreply.github.com>
2022-12-31 05:01:54 +0000

    Pending changes exported from your codespace

---
+++ LineNum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class LineNum {
+    public static void main(String args[]) {
+        String thisline;
+        for (int i = 0; i < args.length; i++) {
+            try {
+                LineNumberReader br = new LineNumberReader(new FileReader("filename.txt"));
+                while ((thisline = br.readLine()) != null) {
+                    System.out.println(br.getLineNumber() + "." + thisline);
+                }
+            } catch (IOException e) {
+                System.out.println("error:" + e);
+            }
+        }
+    }
+}
